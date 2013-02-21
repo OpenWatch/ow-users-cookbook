8ee06ee53f585d6731f5056ec2907f66aa972013
Chris Ballinger <chrisballinger@gmail.com>
2013-02-20 17:55:18 -0800

    maybe this works

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,39 @@
 #
 # Licensed under the AGPLv3
 #
+
+include_recipe "user"
+
+users_databag_name = 'users'
+passwords_databag_name = "user-passwords"
+passwords_item_name = "passwords"
+
+users = data_bag(users_databag_name)
+passwords = Chef::EncryptedDataBagItem.load(passwords_databag_name, passwords_item_name)
+admins = []
+
+users.each do |username|
+  # This causes a round-trip to the server for each admin in the data bag
+  user = data_bag_item(users_databag_name, username)
+
+  if user['gid'] == 999
+    admins.push(username)
+  end
+
+  user_account username do
+    comment   user['comment']
+    ssh_keys  user['ssh_keys']
+    uid       user['uid']
+    gid       user['gid']
+    password  passwords[username]
+    home      home
+  end
+end
+
+# Create an "admins" group on the system
+# You might use this group in the /etc/sudoers file
+# to provide sudo access to the admins
+group "admins" do
+  gid     999
+  members admins
+end
