5f1e0844303f016418df7522e7e92822d8aab5dd
Chris Ballinger <chrisballinger@gmail.com>
2013-02-20 16:09:29 -0800

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'ow_users'
+maintainer       'OpenWatch FPC'
+maintainer_email 'chris@openwatch.net'
+license          'AGPLv3'
+description      'Installs/Configures ow_users'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: ow_users
+# Recipe:: default
+#
+# Copyright 2013, OpenWatch FPC
+#
+# Licensed under the AGPLv3
+#
