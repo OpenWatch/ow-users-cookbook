75b2cbf7963d6f7ca28cc9a0cca60e6d84201809
David Brodsky <davidpbrodsky@gmail.com>
2013-02-22 13:54:37 -0800

    user creation works

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures ow_users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "ow_users", "setup OpenWatch user accounts"
+
 depends "user", "~> 0.3.0"
 depends "sudo"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,23 +23,30 @@ gids = gids_item["gids"]
 passwords = Chef::EncryptedDataBagItem.load(passwords_databag_name, passwords_item_name)
 admins = []
 
+Chef::Log.info("Got users #{users}")
+
 users.each do |username|
   # This causes a round-trip to the server for each admin in the data bag
   user = data_bag_item(users_databag_name, username)
 
+  uid = user['uid']
+  gid = user['gid']
+  Chef::Log.info("Attemping to create user #{username} with id #{uid}")
+
   # If the group is registered in our group data bag, create it
-  if gids.has_key?(user['gid'])
-    group gids.fetch(user['gid']) do
-      gid     user['gid']
+  if gids.has_key?(gid.to_s())
+    group gids.fetch(gid.to_s()) do
+      gid     gid
     end
+    Chef::Log.info("Created group with gid #{gid}")
   end
 
-  if active_gids.include? user['gid']
+  if active_gids.include? gid
     user_account username do
       comment   (defined?(user['comment']) ? user['comment'] : "")
       ssh_keys  (defined?(user['ssh_keys']) ? user['ssh_keys'] : "")
-      uid       user['uid']
-      gid       user['gid']
+      uid       uid
+      gid       gid
       password  passwords[username]
     end
 
